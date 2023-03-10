92cd15b2449de3da36e1c6e02f8cca9dd25e4344
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-03-10 18:17:02 +0100

    create card

---
+++ bin/card.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+const gradient = require("gradient-string");
+
+const card = `
+  ███████████████████████████████████████
+  ██           _                       ██
+  ██    _____ (_)__      Christopher   ██
+  ██   / ___// / _ \\         Ehrlich   ██
+  ██  / /__ / /  __/                   ██
+  ██  \\___// /\\___/                    ██
+  ██    /___/                          ██
+  ██                                   ██
+  ██   ehrlich.christopher@gmail.com   ██
+  ██   c-ehrlich.dev                   ██
+  ██   github              c-ehrlich   ██
+  ██   twitter          ccccjjjjeeee   ██
+  ██                                   ██
+  ███████████████████████████████████████`;
+
+const cardGradient = gradient(
+  Object.values({
+    blue: "#add7ff",
+    magenta: "#fae4fc",
+    red: "#d0679d",
+  })
+);
+
+const cardOutput = cardGradient.multiline(card);
+
+console.log(cardOutput);
